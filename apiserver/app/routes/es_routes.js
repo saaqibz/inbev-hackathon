2fc9086efa66dfa7c92ae5856cc339bb57a95a9a
adepenbusch <adepenbusch@gmail.com>
2017-03-26 11:28:43 -0400

    Hits ES server for API get

--- apiserver/app/routes/es_routes.js
+++ apiserver/app/routes/es_routes.js
@@ -1,12 +1,10 @@
 // routes/es_routes.js.js
 
 function esRoutes(req, res) {
-    let {title, body} = req.body;
-    res.send(`${title}: ${body}`);
+	req.get({url: "http://69.30.216.163:9200/posindex/_search?q=bar_product_id=" +req.params.bar_product_id
+	}, res.send(body));
 }
 
 
 module.exports = (app, db) => {
-    app.post('/elasticsearch', esRoutes);
-    
 };
